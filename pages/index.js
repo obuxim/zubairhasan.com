9378877b66a41da7fd439984412f17932d8bca63
Zubair Hasan <z@duoneos.com>
2021-09-22 00:21:06 +0600

    Fixing no custom font in page

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,28 @@
-import React from 'react'
-import Head from 'next/head'
-import {styles} from '../styles/Home.module.css'
+import React from 'react';
+import Head from 'next/head';
+import { styles } from '../styles/Home.module.css';
 
 const index = () => {
   return (
     <>
       <Head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta charset='UTF-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no' />
         <title>Zubair Hasan | Zubo | Obuxim | Lazy King</title>
-        <meta name="description" content="Personal website of Zubair Hasan, a software engineer by heart and businessman by profession!" />
-        <meta name="keywords" content="Zubo, Zubair, Hasan, Lazy, King, Duoneos, Octoriz, Rempixel, Locafie, Ship & Carry, Bangladeshi Entreprenuer" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap" rel="stylesheet" />
+        <meta
+          name='description'
+          content='Personal website of Zubair Hasan, a software engineer by heart and businessman by profession!'
+        />
+        <meta
+          name='keywords'
+          content='Zubo, Zubair, Hasan, Lazy, King, Duoneos, Octoriz, Rempixel, Locafie, Ship & Carry, Bangladeshi Entreprenuer'
+        />
       </Head>
-      <section className="flex justify-center align-center h-100">
-          If you're here somehow. You already know who I am!
+      <section className='flex justify-center align-center h-100'>
+        If you're here somehow. You already know who I am!
       </section>
     </>
-  )
-}
+  );
+};
 
-export default index
+export default index;
